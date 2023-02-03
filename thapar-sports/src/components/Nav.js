a875cd1c10e826ef7100941f11ca94b1a7e63b82
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-02-03 15:51:23 +0530

    nav added

--- thapar-sports/src/components/Nav.js
+++ thapar-sports/src/components/Nav.js
@@ -4,8 +4,8 @@ export default function Nav() {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"></a>
+  <div class="container">
+    <a id="brand" class="navbar-brand" href="#"><img src="https://www.thapar.edu/images/logo.png" alt="Thapr-sports"></img></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,7 +15,34 @@ export default function Nav() {
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">About us</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Facilitics
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Cricket</a></li>
+            <li><a class="dropdown-item" href="#">Football</a></li>
+            <li><a class="dropdown-item" href="#">Basketball</a></li>
+          </ul>
+          </li>
+          
+          <li class="nav-item">
+          <a class="nav-link" href="#">Achievements</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Faculty</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Events</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-user"></i></a>
         </li>
       </ul>
       
