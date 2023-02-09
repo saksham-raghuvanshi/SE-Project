32babc60089da551b728066275d2db84c51e3c7f
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-02-10 00:45:01 +0530

    update

--- thapar-sports/src/App.js
+++ thapar-sports/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
     <Nav/>
+    <Footer/>
 
     </div>
   );
---
+++ thapar-sports/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <p>Copyright © 2020. Thapar Institute of Engineering & Technology. All rights resrved</p>
+    </div>
+  )
+}
