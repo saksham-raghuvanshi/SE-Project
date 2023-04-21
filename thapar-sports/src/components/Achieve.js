5c710eb8d6b1d2a6fe2ef48b52614d8eb69447ee
lion656544 <100365269+lion656544@users.noreply.github.com>
2023-04-21 11:58:05 +0530

    Achievement Page added

---
+++ thapar-sports/src/components/Achieve.js
@@ -0,0 +1,159 @@
+import React from 'react'
+
+export default function Achieve() {
+    return (
+        <div>
+            <div class="container" id="upperimage">
+                <img src="img/ach1.jpg" alt="Image" style="width:100% ; height:420px" />
+                <div class="centered">Our Achievements</div>
+            </div>
+            <br></br>
+            <div class="my-5">
+                <div class="p-5 text-center bg-body-tertiary">
+                    <div class="container py-5" >
+                        <img src="img/ach10sp-removebg-preview.png" />
+                        <h1 class="text-body-emphasis">Passion and Perseverance</h1>
+                        <br></br>
+                        <p class="col-lg-8 mx-auto lead">
+                            Share your love for the game, and inspire others to follow their passions and pursue their dreams with unwavering determination. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach1sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text"> BASKETBALL TEAM FIRST IN INTER TECH. AND URJA 2019 </p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach2sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text">   BASKETBALL GIRLS TEAT GOT FIRST POSITION IN URJA 2020.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach3sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text"> FOOTBALL TEAM FIRST IN URJA 2019.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach4sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text"> CRICKET TEAM 3rd IN INTER TECH. 2019-20.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach5sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text">Saksham Mahajan (2nd Year, BE-Mechanical) won bronze medal in 55th National Roller Sports Championship held at Kurukshetra, Haryana (18th-23rd January 2018).</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach6sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach7sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text">Suman (4th Year, BE-SE) with a team of 4 students secured 3rd position in 4x100m Relay race after defeating the teams from all across India in the event “Inter Technology University championship” held at Thapar Institute of Engineering & Technology (16th Apr, 2018).</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach9sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text">Maanvi Maini (3rd Year, BE-ENC) won 2 Gold and 1 silver medals in Athletics (200m, 4x100m Relay, 100m) in the event URJA-2018 held at Thapar Institute of Engineering & Technology, Patiala (16th-18th Feb, 2018).</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" src="img/ach9sp.jpg"
+                                alt="Card image cap" />
+                            <div class="card-body">
+                                <p class="card-text">Astha Nigam (3rd Year, BE-COE) won 2nd prize and cash prize of ₹10000 rupees in Basketball in the event URJA-2018 held at Thapar Institute of Engineering & Technology, Patiala (16th-18th Feb, 2018).</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
