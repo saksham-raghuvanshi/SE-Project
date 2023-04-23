6786ff491c127719e5d1cf8b9d77c21accf66ddc
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-04-23 19:56:12 +0530

    button added

--- thapar-sports/src/components/Home.js
+++ thapar-sports/src/components/Home.js
@@ -41,9 +41,9 @@ export default function Home() {
                 <div id="aboutus"><h1>About us</h1>
                 <p>Stay updated with the latest sports events and announcements, book your swimming entries, and issue sports equipment all in one place
                 </p>
-                <button className="btn btn-primary">Read More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                <a href="/about-us"><button className="btn btn-primary" >Read More <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-</svg></button></div>
+</svg></button></a></div>
             </div>
 
 
